60add8e193a69ca8365bb45e9ae86212ac5fbe1e
BlackLabel98 <87873998+BlackLabel98@users.noreply.github.com>
2023-04-08 04:27:03 +0700

    Update Gui.lua

--- Gui.lua
+++ Gui.lua
@@ -143,6 +143,8 @@ local Topbar = Main.Topbar
 local Elements = Main.Elements
 local LoadingFrame = Main.LoadingFrame
 local TabList = Main.TabList
+local SearchBar = Main.Searchbar
+local Filler = SearchBar.CanvasGroup.Filler
 
 Rayfield.DisplayOrder = 100
 LoadingFrame.Version.Text = Release
