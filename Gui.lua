040abbb859a96a7b43698aa7a2af98ab0c6f52f2
BlackLabel98 <87873998+BlackLabel98@users.noreply.github.com>
2023-04-08 18:50:14 +0700

    Update Gui.lua

--- Gui.lua
+++ Gui.lua
@@ -1722,9 +1722,11 @@ function RayfieldLibrary:CreateWindow(Settings)
 			Dropdown.Interact.Position = UDim2.new(0, 233, 0, 21)
 			
 			local function RefreshSelected()
-				if DropdownSettings.Selected == nil then
-					Dropdown.Selected.Text = ""
-				end
+				print("A",tostring(DropdownSettings.Items.Selected[1]))
+				print("B",tostring(DropdownSettings.Items.Selected[1].Option.Name))
+				--if DropdownSettings.Selected == nil then
+				--	Dropdown.Selected.Text = ""
+				--end
 				if #DropdownSettings.Items.Selected > 1 then
 					local NT = {}
 					for _,kj in ipairs(DropdownSettings.Items.Selected) do
