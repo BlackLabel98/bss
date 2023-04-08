9f6436a9bfd4ace859832d532f686192d2644172
BlackLabel98 <87873998+BlackLabel98@users.noreply.github.com>
2023-04-08 18:34:28 +0700

    Update Gui.lua

--- Gui.lua
+++ Gui.lua
@@ -1722,18 +1722,31 @@ function RayfieldLibrary:CreateWindow(Settings)
 			Dropdown.Interact.Position = UDim2.new(0, 233, 0, 21)
 			
 			local function RefreshSelected()
+				if not Multi then
+				print("A",#DropdownSettings.Items.Selected)
 				if #DropdownSettings.Items.Selected > 1 then
 					local NT = {}
 					for _,kj in ipairs(DropdownSettings.Items.Selected) do
 						NT[#NT+1] = kj.Option.Name
 					end
 					Dropdown.Selected.Text = table.concat(NT, ", ")
-					print("A",Dropdown.Selected.Text)
 				elseif DropdownSettings.Items.Selected[1] then
 					Dropdown.Selected.Text = DropdownSettings.Items.Selected[1].Option.Name
-					print("A",DropdownSettings.Items.Selected[1].Option.Name)
 				else
 					Dropdown.Selected.Text = "Select an option"
+				end
+				else
+				if #DropdownSettings.Items.Selected > 1 then
+					local NT = {}
+					for _,kj in ipairs(DropdownSettings.Items.Selected) do
+						NT[#NT+1] = kj.Option.Name
+					end
+					Dropdown.Selected.Text = table.concat(NT, ", ")
+				elseif DropdownSettings.Items.Selected[1] then
+					Dropdown.Selected.Text = DropdownSettings.Items.Selected[1].Option.Name
+				else
+					Dropdown.Selected.Text = "Select an option"
+				end
 				end
 			end
 
